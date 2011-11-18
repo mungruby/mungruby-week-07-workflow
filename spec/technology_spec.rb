eb095eea6d65af1b280233dac94a1f530cc95b07
R. Scott Shaffer <r_scott.shaffer@comcast.net>
2011-11-17 18:19:24 -0800

    added examples for when retired

--- spec/technology_spec.rb
+++ spec/technology_spec.rb
@@ -22,12 +22,9 @@ describe "states" do
     it "should not be publishable when unapproved" do
       expect { subject.publish! }.to raise_error
     end
-
     it "should tell us it is approved" do
-      pending
-      expect {subject.approve!}.to_s == "technology is approved"
+      expect { subject.approve! }.to_s == "technology is approved"
     end
-     
     it "can be set back to unapproved" do
       subject.approve!
       subject.unapprove!
@@ -47,24 +44,59 @@ describe "states" do
       subject.publish!
       subject.current_state.to_s.should == "published"
     end
-
     it "can be set to retired" do
+      subject.approve!
+      subject.publish!
+      expect { subject.retire! }.to_not raise_error
+    end
+  end
+
+  context "when retired" do
+    it "should be retired" do
       subject.approve!
       subject.publish!
       subject.retire!
       subject.retired?.should == true
       subject.current_state.to_s.should == "retired"
     end
-  end
-
-  context "when retired" do
+    it "should tell us it is retired" do
+    pending
+      subject.approve!
+      subject.publish!
+      # expect { subject.retire! }.call == "technology is retareded"
+      expect { subject.retire! }.call.should == "retired"
+      # should_receive(:puts).once
+    end
+    it "there should be no available events" do
+      subject.approve!
+      subject.publish!
+      subject.retire!
+      subject.current_state.events.keys.should == []
+    end
+    it "cannot be set to approved" do
+      subject.approve!
+      subject.publish!
+      subject.retire!
+      expect { subject.approve! }.to raise_error
+    end
+    it "cannot be set to unapproved" do
+      subject.approve!
+      subject.publish!
+      subject.retire!
+      expect { subject.unapprove! }.to raise_error
+    end
     it "cannot be set to published" do
       subject.approve!
       subject.publish!
       subject.retire!
       expect { subject.publish! }.to raise_error
     end
+    it "cannot be set to retired" do
+      subject.approve!
+      subject.publish!
+      subject.retire!
+      expect { subject.retire! }.to raise_error
+    end
   end
-
   
 end
