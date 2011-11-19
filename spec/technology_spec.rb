1c8d4f505d8a842957a794bcc3dfeb8f1a255f94
R. Scott Shaffer <r_scott.shaffer@comcast.net>
2011-11-18 17:05:31 -0800

    completed specs - 100% coverage

--- spec/technology_spec.rb
+++ spec/technology_spec.rb
@@ -1,7 +1,7 @@
 # require "rubygems"
 # require "bundler/setup"
-# require 'simplecov'
-# SimpleCov.start
+require 'simplecov'
+SimpleCov.start
 # require File.expand_path(File.dirname(__FILE__) + '/../lib/technology.rb')
 require_relative '../lib/technology.rb'
 
@@ -13,6 +13,54 @@ describe "states" do
     subject.current_state.to_s.should == "unapproved"
   end
 
+
+  #
+  # UNAPPROVED
+  #
+  context "when unapproved" do
+
+    it "should be unapproved" do
+      subject.unapproved?.should == true
+      subject.current_state.to_s.should == "unapproved"
+    end
+
+    it "should tell us it is unapproved when set to unapproved" do
+      # pending
+      subject.approve!
+      subject.unapprove!
+    end
+
+    it "cannot be set to unapproved" do
+      expect { subject.unapprove! }.to raise_error
+    end
+
+    it "available events should be approve" do
+      subject.current_state.events.should have_key(:approve)
+      subject.current_state.events.keys.should == [:approve]
+    end
+
+    it "can be set to approved" do
+      expect { subject.approve! }.to_not raise_error
+      subject.approved?.should == true
+      subject.current_state.to_s.should == "approved"
+    end
+
+    it "cannot be set to published" do
+      expect { subject.publish! }.to raise_error
+      subject.unapproved?.should == true
+    end
+
+    it "cannot be set to retired" do
+      expect { subject.retire! }.to raise_error
+      subject.unapproved?.should == true
+    end
+
+  end
+
+
+  #
+  # APPROVED
+  #
   context "when approved" do
     it "should be approved" do
       subject.approve!
@@ -22,70 +70,116 @@ describe "states" do
     it "should not be publishable when unapproved" do
       expect { subject.publish! }.to raise_error
     end
-    it "should tell us it is approved" do
-      expect { subject.approve! }.to_s == "technology is approved"
+
+    it "should tell us it is approved when set to approved" do
+      # pending
+      # expect { subject.approve! }.to_s == "technology is approved"
+      expect { subject.approve! }.call.should == "approved"
+      subject.approved?.should == true
     end
-    it "can be set back to unapproved" do
-      subject.approve!
-      subject.unapprove!
-      subject.current_state.to_s.should == "unapproved"
-    end
+
     it "available events should be publish and unapprove" do
       subject.approve!
       subject.current_state.events.should have_key(:publish)
       subject.current_state.events.should have_key(:unapprove)
       subject.current_state.events.keys.should == [:publish,:unapprove]
     end
+
+    it "can be set back to unapproved" do
+      subject.approve!
+      subject.approved?.should == true
+      subject.unapprove!
+      subject.current_state.to_s.should == "unapproved"
+    end
+
+    it "can be set back to published" do
+      subject.approve!
+      subject.approved?.should == true
+      subject.publish!
+      subject.current_state.to_s.should == "published"
+    end
+
+    it "cannot be set to approved when already approved" do
+      subject.approve!
+      subject.approved?.should == true
+      expect { subject.approve! }.to raise_error
+    end
+     
+    it "cannot be set to retired" do
+      subject.approve!
+      subject.approved?.should == true
+      expect { subject.retire! }.to raise_error
+      subject.approved?.should == true
+    end
   end
+
 
   #
   # PUBLISHED
   #
   context "when published" do
+     
     it "should be published" do
       subject.approve!
       subject.publish!
       subject.published?.should == true
       subject.current_state.to_s.should == "published"
     end
-    it "should tell us it is published" do
-      pending
+     
+    it "should tell us it is published when set to published" do
+      # pending
       subject.approve!
-      subject.publish!
+      expect { subject.publish! }.call.should == "published"
+      subject.published?.should == true
     end
+     
     it "available events should be retire" do
       subject.approve!
       subject.publish!
+      subject.published?.should == true
       subject.current_state.events.should have_key(:retire)
       subject.current_state.events.keys.should == [:retire]
     end
-    it "cannot be set to approved" do
-      subject.approve!
-      subject.publish!
-      subject.retire!
-      expect { subject.approve! }.to raise_error
-    end
-    it "cannot be set to unapproved" do
-      subject.approve!
-      subject.publish!
-      expect { subject.unapprove! }.to raise_error
-    end
-    it "cannot be set to published" do
-      subject.approve!
-      subject.publish!
-      expect { subject.publish! }.to raise_error
-    end
+     
     it "can be set to retired" do
       subject.approve!
       subject.publish!
+      subject.published?.should == true
       expect { subject.retire! }.to_not raise_error
+      subject.retired?.should == true
     end
+
+    it "cannot be set to published when already published" do
+      subject.approve!
+      subject.publish!
+      subject.published?.should == true
+      expect { subject.publish! }.to raise_error
+    end
+     
+    it "cannot be set to approved" do
+      subject.approve!
+      subject.publish!
+      subject.published?.should == true
+      expect { subject.approve! }.to raise_error
+      subject.published?.should == true
+    end
+     
+    it "cannot be set to unapproved" do
+      subject.approve!
+      subject.publish!
+      subject.published?.should == true
+      expect { subject.unapprove! }.to raise_error
+      subject.published?.should == true
+    end
+
   end
+
 
   #
   # RETIRED
   #
   context "when retired" do
+
     it "should be retired" do
       subject.approve!
       subject.publish!
@@ -93,44 +187,57 @@ describe "states" do
       subject.retired?.should == true
       subject.current_state.to_s.should == "retired"
     end
-    it "should tell us it is retired" do
+
+    it "should tell us it is retired when set to retired" do
       # pending
       subject.approve!
       subject.publish!
       expect { subject.retire! }.call.should == "retired"
-      # subject.retire!
-      # should_receive(:puts).once
+      subject.retired?.should == true
     end
+
     it "there should be no available events" do
       subject.approve!
       subject.publish!
       subject.retire!
+      subject.retired?.should == true
       subject.current_state.events.keys.should == []
     end
-    it "cannot be set to approved" do
-      subject.approve!
-      subject.publish!
-      subject.retire!
-      expect { subject.approve! }.to raise_error
-    end
-    it "cannot be set to unapproved" do
-      subject.approve!
-      subject.publish!
-      subject.retire!
-      expect { subject.unapprove! }.to raise_error
-    end
-    it "cannot be set to published" do
-      subject.approve!
-      subject.publish!
-      subject.retire!
-      expect { subject.publish! }.to raise_error
-    end
-    it "cannot be set to retired" do
+
+    it "cannot be set to retired when already retired" do
       subject.approve!
       subject.publish!
       subject.retire!
       expect { subject.retire! }.to raise_error
     end
+
+    it "cannot be set to approved" do
+      subject.approve!
+      subject.publish!
+      subject.retire!
+      subject.retired?.should == true
+      expect { subject.approve! }.to raise_error
+      subject.retired?.should == true
+    end
+
+    it "cannot be set to unapproved" do
+      subject.approve!
+      subject.publish!
+      subject.retire!
+      subject.retired?.should == true
+      expect { subject.unapprove! }.to raise_error
+      subject.retired?.should == true
+    end
+
+    it "cannot be set to published" do
+      subject.approve!
+      subject.publish!
+      subject.retire!
+      subject.retired?.should == true
+      expect { subject.publish! }.to raise_error
+      subject.retired?.should == true
+    end
+
   end
 
 end
